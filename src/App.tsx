c16e485ec4fcfe3658126057df7c81194869763f
ginavasiloff <ginavasiloff@gmail.com>
2019-01-13 10:26:06 -0700

    add more mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import * as React from "react";
 
-import { Card } from "@rmwc/card";
+import {
+  Card,
+  CardAction,
+  CardActions,
+  CardActionButtons,
+  CardMedia,
+  CardMediaContent
+} from "@rmwc/card";
 import { Grid, GridCell } from "@rmwc/grid";
 import { Typography } from "@rmwc/typography";
 
 import { Recipe } from "./recipe";
-import { mockRecipe } from "./recipe-defs";
+import { mockRecipes } from "./recipe-defs";
 
 import "material-components-web/dist/material-components-web.min.css";
 import "./App.css";
 
-const recipes = [mockRecipe, mockRecipe, mockRecipe];
+const recipes = mockRecipes;
 
 class App extends React.Component {
   render() {
@@ -20,7 +27,38 @@ class App extends React.Component {
         <Grid>
           {recipes.map(r => (
             <GridCell span={4}>
-              <Card>{r.name}</Card>
+              <Card>
+                <CardMedia
+                  sixteenByNine
+                  style={{ backgroundImage: `url(${r.image})` }}
+                >
+                  <CardMediaContent>
+                    <Typography
+                      use="headline4"
+                      theme="text-primary-on-dark"
+                      style={{
+                        padding: "0.5rem 1rem",
+                        backgroundImage:
+                          "linear-gradient(to bottom, transparent 0%, rgba(0, 0, 0, 0.55) 75%)",
+                        bottom: "0",
+                        left: "0",
+                        right: "0",
+                        position: "absolute",
+                        textTransform: "capitalize",
+                        textAlign: "left"
+                      }}
+                    >
+                      {r.name}
+                    </Typography>
+                  </CardMediaContent>
+                </CardMedia>
+                <CardActions>
+                  <CardActionButtons>
+                    <CardAction>ingredients</CardAction>
+                    <CardAction>full recipe</CardAction>
+                  </CardActionButtons>
+                </CardActions>
+              </Card>
             </GridCell>
           ))}
         </Grid>
