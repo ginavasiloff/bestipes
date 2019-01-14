dadf704ea55eecd1eee658880c852b2d6c7b47c1
ginavasiloff <ginavasiloff@gmail.com>
2019-01-14 09:56:39 -0700

    added a home button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import { RecipeGrid } from "./views/recipe-grid";
 import { Recipe } from "./views/recipe";
 import { mockRecipes } from "./recipe-defs";
 
 import { MatchT } from "./app-defs";
+
+import {
+  Toolbar,
+  ToolbarRow,
+  ToolbarSection,
+  ToolbarTitle
+} from "@rmwc/toolbar";
+import { Typography } from "@rmwc/typography";
 
 import "material-components-web/dist/material-components-web.min.css";
 import "./App.css";
@@ -16,25 +24,40 @@ class App extends React.Component {
     return (
       <div className="App">
         <Router>
-          <div>
-            <Route
-              exact
-              path="/"
-              component={({ match }: { match: MatchT }) => (
-                <RecipeGrid match={match} recipes={mockRecipes} />
-              )}
-            />
-            <Route
-              path="/recipe/:name"
-              component={({ match }: { match: MatchT }) => (
-                <Recipe
-                  recipe={mockRecipes.find(
-                    r => slugify(r.name) === match.params.name
-                  )}
-                />
-              )}
-            />
-          </div>
+          <React.Fragment>
+            <Toolbar>
+              <ToolbarRow>
+                <ToolbarSection>
+                  <ToolbarTitle>
+                    <Link to="/">
+                      <Typography use="headline4" theme="text-primary-on-dark">
+                        Bestipes
+                      </Typography>
+                    </Link>
+                  </ToolbarTitle>
+                </ToolbarSection>
+              </ToolbarRow>
+            </Toolbar>
+            <div>
+              <Route
+                exact
+                path="/"
+                component={({ match }: { match: MatchT }) => (
+                  <RecipeGrid match={match} recipes={mockRecipes} />
+                )}
+              />
+              <Route
+                path="/recipe/:name"
+                component={({ match }: { match: MatchT }) => (
+                  <Recipe
+                    recipe={mockRecipes.find(
+                      r => slugify(r.name) === match.params.name
+                    )}
+                  />
+                )}
+              />
+            </div>
+          </React.Fragment>
         </Router>
       </div>
     );
--- src/views/recipe.tsx
+++ src/views/recipe.tsx
@@ -1,21 +1,39 @@
 import * as React from "react";
+
 import { Typography } from "@rmwc/typography";
-import { List, SimpleListItem } from "@rmwc/list";
+import { List, ListItem, SimpleListItem } from "@rmwc/list";
 import { RecipeT } from "../recipe-defs";
+
+import styles from "./recipe.module.css";
 
 export const Recipe = ({ recipe }: { recipe: RecipeT | undefined }) => {
   return !!recipe ? (
     <React.Fragment>
-      <Typography use="headline3">{recipe.name}</Typography>
-      <List twoLine>
-        {recipe.ingredients.map((i, idx) => (
-          <SimpleListItem
-            id={`${idx}-${i.name.replace(" ", "-")}`}
-            secondaryText={i.details}
-            text={`${i.quantity} ${i.name}`}
-          />
-        ))}
-      </List>
+      <Typography use="headline4">{recipe.name}</Typography>
+      <div className={styles.recipe}>
+        <div className={styles.ingredients}>
+          <Typography use="headline6">Ingredients</Typography>
+          <List twoLine>
+            {recipe.ingredients.map((i, idx) => (
+              <SimpleListItem
+                id={`${idx}-${i.name.replace(" ", "-")}`}
+                secondaryText={i.details}
+                text={`${i.quantity} ${i.name}`}
+              />
+            ))}
+          </List>
+        </div>
+        <div className={styles.instructions}>
+          <Typography use="headline6">Instructions</Typography>
+          <div className={styles.steps}>
+            {recipe.instructions.map((i, idx) => (
+              <Typography tag="p" key={`step-${idx}`} use="body1">
+                {i}
+              </Typography>
+            ))}
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   ) : (
     <div>no recipe found</div>
